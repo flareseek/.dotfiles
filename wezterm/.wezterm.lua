5e9d00154f07e77e502a3230e109d41d0372091e
flareseek <flareseek@gmail.com>
2025-06-24 01:19:54 +0900

    fix: wezterm size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,8 +1,8 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.initial_cols = 300
-config.initial_rows = 300
+config.initial_cols = 120
+config.initial_rows = 28
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
