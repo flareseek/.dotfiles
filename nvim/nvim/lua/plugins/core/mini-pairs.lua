64c4098c652c98ddf69a4b5fe98503850605e7f4
flareseek <flareseek@gmail.com>
2025-09-10 16:47:39 +0900

    update

--- nvim/nvim/lua/plugins/core/mini-pairs.lua
+++ nvim/nvim/lua/plugins/core/mini-pairs.lua
@@ -1,5 +1,5 @@
 return {
-  'echasnovski/mini.pairs',
+  'nvim-mini/mini.pairs',
   version = false,
   -- Currently, if Telescope is loaded first, line breaks aren’t applied properly.
   -- event = "InsertEnter",
